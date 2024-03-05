8a4c6d783470a325e835422b309ed2a028d875b3
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-05 23:17:00 +0600

    challange python for 100 days

---
+++ band_generator.py
@@ -0,0 +1,17 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+print("Welcome to the band club")
+city =input("Which city you grew up")
+pet =input("What is your pet name?")
+print("The band name is " + city + " " +pet)
+
+# street_name = "Abbey Road"
+# print(street_name[4] + street_name[7])
---
+++ day-2_type.py
@@ -0,0 +1,5 @@
+num_char = len(input("what is your name?"))
+
+con_str = str(num_char)
+
+print("my name is " + con_str +", i am from bangladesh")
