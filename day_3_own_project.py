5f37e6d48f40be1a3cbdcf818f6cfd7d678f7fde
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-23 23:41:19 +0600

    day_3_own_project

---
+++ day_3_own_project.py
@@ -0,0 +1,30 @@
+print("Mission Is going to port city international university as soon as early")
+print("Welcome to PCIU!..")
+
+choice1 = input('First you choose which vehicle you start your journey? if you go to easily then type."cng" or "tom_tom"')
+
+if choice1 == "cng":
+  #continue
+  choice2 = input('If you are arrive at fokirnir hut .Then which vehicle you choose .you are directly go new bridge then Type. "bus" else Type."cng"')
+  
+  if choice2 == "bus":
+    #continue
+    choice3 = input('If you reach new bridge.Then which side you go.option 1 Type. "gec" and option 2 Type . "market"')
+    if choice3 == "market":
+      # continue
+      choice4 = input('if you go  market to your campus. which vehicle you choos opt1 Type. bike opt2 type. tempu and opt3 type bus')
+      if choice4 == "tempu":
+        print("Then you change 3/4 vehicle so that you are frustrated")
+      elif choice4 == "bus":
+        print("The vehicle is too slow that you cannot reach you campus at your class time")
+      else:
+        print("You are reach your campus too early....!")
+      
+    else:
+      print("You have face too much jam")
+  else:
+    print("You have to change too much vehicle .That you are reaching your goal lately!")
+  
+
+
+  
